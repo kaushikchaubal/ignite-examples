5c3b732ac22e4b07b5b1b5f729cc13c27139176a
Kaushik <kaushikchaubal@users.noreply.github.com>
2016-07-12 14:18:37 +0100

    Updating HelloWorldCompute

--- HelloWorldCompute.java
+++ HelloWorldCompute.java
@@ -1,7 +1,5 @@
-package com.ignite.examples;
+package com.bfm.app.ignite.meetup;
 
-import org.apache.ignite.Ignite;
-import org.apache.ignite.Ignition;
 import org.junit.Test;
 
 
@@ -10,12 +8,9 @@ public class HelloWorldCompute {
 
 	@Test
 	public void helloWorldCompute() throws InterruptedException {
-	    Ignition.setClientMode(true);
-	    try (Ignite ignite = Ignition.start()) {
-	        ignite.compute().broadcast(() -> System.out.println("Hello World"));
-	        
-	        Thread.sleep(20000);
-	           ignite.compute().broadcast(() -> System.out.println("Hello World"));
-	    }
+
+		try (Ignite ignite = Ignition.start()) {
+			ignite.compute().broadcast(() -> System.out.println("Hello World"));
+		}
 	}
 }
