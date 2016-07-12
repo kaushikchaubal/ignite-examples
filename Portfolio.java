0a732034b7eee5ea149cd3c85cdc4dccab1e4869
Kaushik <kaushikchaubal@users.noreply.github.com>
2016-07-12 14:21:04 +0100

    Updating Position

--- Portfolio.java
+++ Portfolio.java
@@ -1,130 +1,83 @@
 package com.bfm.app.ignite.meetup;
 
-import java.util.Collections;
-import java.util.List;
+public class Position {
 
-import org.apache.ignite.cache.query.annotations.QuerySqlField;
-import org.apache.ignite.cache.query.annotations.QueryTextField;
 
-public class Portfolio {
-
-    @QuerySqlField(index = true)
     private final Long id;
 
-    @QueryTextField
-    @QuerySqlField(index = true)
-    private final String name;
+    private final Long portfolioid;
 
-    @QueryTextField
-    private String description;
+    private final String cusip;
 
-    @QuerySqlField
-    private final Double nav;
-
-    private final List<Position> positions;
-
-    public List<Position> getPositions() {
-        return positions;
-    }
-
-    public Long getid() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getNav() {
-        return nav;
-    }
+    private double value;
 
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Portfolio(Long id, String name, double nav, List<Position> positions) {
+    public Position(Long id, Long portfolioid, String cusip, double value) {
         this.id = id;
-        this.name = name;
-        this.description = null;
-        this.nav = nav;
-        this.positions = positions;
+        this.portfolioid = portfolioid;
+        this.cusip = cusip;
+        this.value = value;
     }
 
-    public Portfolio(Long id, String name, String description, double nav, List<Position> positions) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.nav = nav;
-        this.positions = positions;
+    public String getCusip() {
+        return cusip;
+    }
+    public double getValue() {
+        return value;
     }
 
-    public Portfolio(Long id, String name, String description) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.nav = null;
-        this.positions = Collections.emptyList();
+
+
+    public void setValue(double value) {
+        this.value = value;
     }
 
-	@Override
+    @Override
     public String toString() {
-        return "Portfolio [portfolioid=" + id + ", name=" + name + ", nav=" + nav + ", positions=" + positions + "]";
+        return "Position [cusip=" + cusip + ", quantity=" + value + "]";
     }
 
     @Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result
-				+ ((description == null) ? 0 : description.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((nav == null) ? 0 : nav.hashCode());
-		result = prime * result
-				+ ((positions == null) ? 0 : positions.hashCode());
-		return result;
-	}
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((cusip == null) ? 0 : cusip.hashCode());
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((portfolioid == null) ? 0 : portfolioid.hashCode());
+        long temp;
+        temp = Double.doubleToLongBits(value);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 
     @Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Portfolio other = (Portfolio) obj;
-		if (description == null) {
-			if (other.description != null)
-				return false;
-		} else if (!description.equals(other.description))
-			return false;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
-		if (nav == null) {
-			if (other.nav != null)
-				return false;
-		} else if (!nav.equals(other.nav))
-			return false;
-		if (positions == null) {
-			if (other.positions != null)
-				return false;
-		} else if (!positions.equals(other.positions))
-			return false;
-		return true;
-	}
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Position other = (Position) obj;
+        if (cusip == null) {
+            if (other.cusip != null)
+                return false;
+        } else if (!cusip.equals(other.cusip))
+            return false;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (portfolioid == null) {
+            if (other.portfolioid != null)
+                return false;
+        } else if (!portfolioid.equals(other.portfolioid))
+            return false;
+        if (Double.doubleToLongBits(value) != Double.doubleToLongBits(other.value))
+            return false;
+        return true;
+    }
+
+
 }
